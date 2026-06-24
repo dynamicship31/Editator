a051c2df5d28557a86286d51a0f971d6e985efa4
dynamicship31 <conenandrea11@gmail.com>
2026-06-24 18:36:40 +0200

    Delete terminalUtils.py

--- terminalUtils.py
+++
@@ -1,9 +0,0 @@
-def printAt(x:int, y:int, string:str):
-    print(f"\033[H\033[{x};{y}H{string}",end="")
-
-def cursorVisible(x:bool):
-    if x == False : print(f"\x1b[?25l",end="")
-    else : print(f"\x1b[?25h",end="")
-
-def cursorAt(x,y):
-    print(f"\033[H\033[{x};{y}H\033[4m\033[{x+1};{y}H\033[0m",end="")
